73e21b9d100a6fb6f3c71d9f97c114ad66005ac0
Mohammad Arabzadeh <a.mohamad7824@gmail.com>
2022-06-03 14:53:47 +0430

    added a gauge to prometheus for cpu_percent

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 import socket
-
+import json
+from prometheus_client import start_http_server, Gauge
 
 HOST = socket.gethostname()
 PORT = 8080
+PROMETHEUS_PORT = 8000
+g = Gauge("cpu_percent", "CPU usage percentage of the client")
+g.set(0.0)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
+    start_http_server(PROMETHEUS_PORT)
+
     while True:
         conn, addr = s.accept()
         with conn:
@@ -15,7 +21,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 data = conn.recv(1024)
                 if not data:
                     break
-                print(data.decode())
+                msg = json.loads(data.decode())
+                print(msg)
+                g.set(msg["cpu_percent"])
                 conn.sendall("ACK".encode())
         print("Disconnected")
 
