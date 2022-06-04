fa946bab2f01f6d4a064beef0fabf10789a53891
Mohammad Arabzadeh <a.mohamad7824@gmail.com>
2022-06-04 18:31:48 +0430

    added new agent to client.py

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import threading
 import socket
 import psutil
 import time
@@ -6,12 +7,40 @@ import json
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 8080
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while True:
-        cpu_percent = psutil.cpu_percent()
-        msg = dict(cpu_percent=cpu_percent)
-        msg_json = json.dumps(msg)
-        s.sendall(msg_json.encode())
-        print(s.recv(1024).decode())
-        time.sleep(1)
+
+class Agent:
+    def __init__(self, name, val_generator, default=None):
+        self.name = name
+        self.val = val_generator
+        if default is None:
+            val = self.val()
+        else:
+            val = default
+        self.msg = json.dumps({self.name: val})
+
+    def run(self, interval=1):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            while True:
+                s.sendall(self.msg.encode())
+                response = json.loads(s.recv(1024).decode())
+                self.msg = json.dumps({self.name: self.val(**response)})
+                print(f"[{self.name}] {response}")
+                time.sleep(interval)
+
+
+if __name__ == "__main__":
+    agents = []
+    agents.append(
+        Agent("agent_cpu_usage_percent", lambda *args, **kwargs: psutil.cpu_percent())
+    )
+    agents.append(
+        Agent(
+            "agent_delay_seconds",
+            lambda host_time, *args, **kwargs: time.time() - host_time,
+            default=0,
+        )
+    )
+
+    for agent in agents:
+        threading.Thread(target=agent.run, args=()).start()
--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 import threading
 import socket
 import json
+import time
 from prometheus_client import start_http_server, Gauge
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 8080
 PROMETHEUS_PORT = 8000
-g = Gauge("cpu_percent", "CPU usage percentage of the client")
+g = Gauge("agent_cpu_percent", "CPU usage percentage of the client")
 g.set(0.0)
 
 
@@ -14,22 +15,24 @@ def client_thread(conn, addr):
     with conn:
         print(f"[CONNECTION] {addr} connected.")
         while True:
-            data = conn.recv(1024)
+            data = conn.recv(2048)
             if not data:
                 break
             msg = json.loads(data.decode())
             print(f"[{addr[0]}:{addr[1]}] {msg}")
             # g.set(msg["cpu_percent"])
-            conn.sendall("ACK".encode())
+            conn.sendall(
+                json.dumps({"host_time": time.time(), "status": "ACK"}).encode()
+            )
     print(f"[CONNECTION] {addr} disconnected.")
 
 
 if __name__ == "__main__":
+    start_http_server(PROMETHEUS_PORT)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         print(f"[LISTENING] Server is listening on {HOST}:{PORT}")
-        # start_http_server(PROMETHEUS_PORT)
 
         while True:
             conn, addr = s.accept()
